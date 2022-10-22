b6c7c8600fa535b75aa9db4cbb53fa982a4f25aa
dilusha07 <dilushasandaruwani07@gmail.com>
2022-10-23 01:45:17 +0530

    Implement Add task component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import InputTask from "./components/inputtask";
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloo....</h1>
+      <InputTask />
     </div>
   );
 }
